0697c0c918d771dc09a518871564e3cdcc8063bb
MightyBeast <zgpoter@gmail.com>
2023-05-11 18:50:33 +0300

    -refactor- : Moved count methods from view to view model.

--- ViewModels/ProjectViewModel.cs
+++ ViewModels/ProjectViewModel.cs
@@ -1,4 +1,6 @@
 using iBugged.Models;
+using iBugged.Models.Enums;
+using Newtonsoft.Json;
 
 namespace iBugged.ViewModels;
 
@@ -7,4 +9,28 @@ public class ProjectViewModel
     public Project project { get; set; } = null!;
     public List<User> members { get; set; } = new List<User>();
     public List<Ticket> tickets { get; set; } = new List<Ticket>();
+
+    public string GetTicketTypesCountJson() =>
+        JsonConvert.SerializeObject(new[]
+        {
+            tickets.Count(ticket => ticket.ticketType == TicketType.Bug),
+            tickets.Count(ticket => ticket.ticketType == TicketType.Task),
+            tickets.Count(ticket => ticket.ticketType == TicketType.Support),
+            tickets.Count(ticket => ticket.ticketType == TicketType.Other)
+        });
+
+    public string GetTicketPriorityCountJson() =>
+        JsonConvert.SerializeObject(new[]
+        {
+            tickets.Count(ticket => ticket.priority == Priority.Low),
+            tickets.Count(ticket => ticket.priority == Priority.Medium),
+            tickets.Count(ticket => ticket.priority == Priority.High)
+        });
+
+    public string GetTicketStatusCountJson() =>
+        JsonConvert.SerializeObject(new[]
+        {
+            tickets.Count(ticket => !ticket.isOpened),
+            tickets.Count(ticket => ticket.isOpened)
+        });
 }
