826a8a4c3c7a0aefafd60148bb610ba1e9093835
MightyBeast <zgpoter@gmail.com>
2023-05-11 19:10:23 +0300

    -refactor- : Moved ticket data related stuff into project view model.

--- ViewModels/ProjectViewModel.cs
+++ ViewModels/ProjectViewModel.cs
@@ -1,6 +1,5 @@
 using iBugged.Models;
 using iBugged.Models.Enums;
-using Newtonsoft.Json;
 
 namespace iBugged.ViewModels;
 
@@ -10,27 +9,47 @@ public class ProjectViewModel
     public List<User> members { get; set; } = new List<User>();
     public List<Ticket> tickets { get; set; } = new List<Ticket>();
 
-    public string GetTicketTypesCountJson() =>
-        JsonConvert.SerializeObject(new[]
+    public string[] GetTicketTypes() =>
+        System.Enum.GetNames(typeof(TicketType));
+
+    public string[] GetTicketTypesColors() =>
+        new [] {
+            "rgb(255, 0, 0)", "rgb(54, 162, 235)",
+            "rgb(235, 192, 0)", "rgb(194, 194, 194)"
+        };
+
+    public int[] GetTicketTypesCount() =>
+        new[]
         {
             tickets.Count(ticket => ticket.ticketType == TicketType.Bug),
             tickets.Count(ticket => ticket.ticketType == TicketType.Task),
             tickets.Count(ticket => ticket.ticketType == TicketType.Support),
             tickets.Count(ticket => ticket.ticketType == TicketType.Other)
-        });
+        };
 
-    public string GetTicketPriorityCountJson() =>
-        JsonConvert.SerializeObject(new[]
+    public string[] GetTicketPriorities() =>
+        System.Enum.GetNames(typeof(Priority));
+
+    public string[] GetTicketPriorityColors() =>
+        new[] { "rgb(33, 196, 0)", "rgb(235, 192, 0)", "rgb(255, 0, 0)" };
+
+    public int[] GetTicketPriorityCount() =>
+        new[]
         {
             tickets.Count(ticket => ticket.priority == Priority.Low),
             tickets.Count(ticket => ticket.priority == Priority.Medium),
             tickets.Count(ticket => ticket.priority == Priority.High)
-        });
+        };
 
-    public string GetTicketStatusCountJson() =>
-        JsonConvert.SerializeObject(new[]
+    public string[] GetTicketStatuses() => new[] { "Closed", "Opened" };
+
+    public string[] GetTicketStatusColors() =>
+        new[] { "rgb(33, 196, 0)", "rgb(255, 0, 0)" };
+
+    public int[] GetTicketStatusCount() =>
+        new[]
         {
             tickets.Count(ticket => !ticket.isOpened),
             tickets.Count(ticket => ticket.isOpened)
-        });
+        };
 }
