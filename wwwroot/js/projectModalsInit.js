fc194c2069bd96fbd24b8bd824f1f4d72a7a8495
MightyBeast <zgpoter@gmail.com>
2023-05-13 18:23:17 +0300

    -refactor- : Refactored project modals scripts.

---
+++ wwwroot/js/projectModalsInit.js
@@ -0,0 +1,36 @@
+function createProject()
+{
+    $.ajax({
+        type: "GET",
+        url: '/Projects/Create',
+        contentType: "application/text; charset=utf-8",
+        success: function (data) {
+            $("#project-create-modal").modal("show");
+            $('.modal-body').html(data);
+            applyMultiSelect();
+        }
+    })
+}
+
+function editProject(projectId)
+{
+    $.ajax({
+        type: "GET",
+        url: '/Projects/Edit?id=' + projectId,
+        contentType: "application/text; charset=utf-8",
+        success: function (data) {
+            $("#project-edit-modal").modal("show");
+           $('.modal-body').html(data);            
+            applyMultiSelect();
+        }
+    })
+}
+
+function applyMultiSelect()
+{
+    $('.my-select').multiSelect(
+    {
+        selectableHeader: "<div class='custom-header'>Select members:</div>",
+        selectionHeader: "<div class='custom-header'>Selected memebers:</div>"
+    })
+}
