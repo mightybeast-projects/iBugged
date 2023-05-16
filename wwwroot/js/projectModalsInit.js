2ec5a5ad1f8160827d2d3d0a9b7fb577f66eec2d
MightyBeast <zgpoter@gmail.com>
2023-05-16 20:25:54 +0300

    -refactor- : Small js refactoring.

--- wwwroot/js/projectModalsInit.js
+++ wwwroot/js/projectModalsInit.js
@@ -1,4 +1,4 @@
-function createProject()
+function createProject(username)
 {
     $.ajax({
         type: "GET",
@@ -7,12 +7,12 @@ function createProject()
         success: function (data) {
             $("#project-create-modal").modal("show");
             $('.modal-body').html(data);
-            applyMultiSelect();
+            applyMultiSelect(username);
         }
     })
 }
 
-function editProject(projectId)
+function editProject(projectId, username)
 {
     $.ajax({
         type: "GET",
@@ -20,17 +20,20 @@ function editProject(projectId)
         contentType: "application/text; charset=utf-8",
         success: function (data) {
             $("#project-edit-modal").modal("show");
-           $('.modal-body').html(data);            
-            applyMultiSelect();
+            $('.modal-body').html(data);            
+            applyMultiSelect(username);
         }
     })
 }
 
-function applyMultiSelect()
+function applyMultiSelect(username)
 {
     $('.my-select').multiSelect(
     {
         selectableHeader: "<div class='custom-header'>Select members:</div>",
         selectionHeader: "<div class='custom-header'>Selected memebers:</div>"
     })
+
+    $('.my-select').multiSelect('select', 'MightyBeast');
+    $('.my-select').multiselect('refresh');
 }
